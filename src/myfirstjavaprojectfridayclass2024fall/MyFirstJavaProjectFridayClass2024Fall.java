966101983549a73542a51196da679aaee4d194a6
NagmaKhattar <nagmakhattar@gmail.com>
2024-09-13 10:03:48 -0400

    Initial commit -added a comment

---
+++ src/myfirstjavaprojectfridayclass2024fall/MyFirstJavaProjectFridayClass2024Fall.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myfirstjavaprojectfridayclass2024fall;
+
+/**
+ *
+ * @author HP
+ */
+public class MyFirstJavaProjectFridayClass2024Fall {
+
+    /**
+     * @param args
+     * the command
+     * line
+     * arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("HEllo");//making a change
+    }
+    
+}
